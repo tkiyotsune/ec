132b1ce3bb0ac128a77b142c846c8f29b54e5583
tkiyotsune <tkiyotsune@gmail.com>
2020-11-04 18:08:12 +0900

    マルチAUTH（修正）

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -31,7 +31,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+     protected $namespace = 'App\Http\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
