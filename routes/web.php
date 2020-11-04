d44ae7731c7eff6e6be41ecf6e40b2de58777454
tkiyotsune <tkiyotsune@gmail.com>
2020-11-04 17:38:48 +0900

    マルチAUTH（接続確認用ルート）

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 // ユーザー
 Route::namespace('user')->prefix('user')->name('user.')->group(function () {
 
